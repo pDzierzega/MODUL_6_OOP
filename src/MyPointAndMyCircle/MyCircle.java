526316e27bf92de7b19007e01d2edffcec34a8f3
Paweł Dzierżęga <pawel.dzierzega@outlook.com>
2017-08-21 20:18:04 +0200

    dodano klasy MyPoint() oraz MyCicrcle , bez testów

---
+++ src/MyPointAndMyCircle/MyCircle.java
@@ -0,0 +1,77 @@
+package MyPointAndMyCircle;
+
+public class MyCircle {
+    private MyPoint center=new MyPoint(0,0);
+    private int radius=1;
+
+    public MyCircle() {}
+
+    public MyCircle(int x,int y,int radius){
+        this.center=new MyPoint(x,y);
+        this.radius=radius;
+    }
+
+    public MyCircle(MyPoint center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public MyPoint getCenter() {
+        return center;
+    }
+
+    public void setCenter(MyPoint center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getCenterX(){
+        return center.getX();
+    }
+
+    public void setCenterX(int x){
+        center.setX(x);
+    }
+
+    public int getCenterY(){
+        return center.getY();
+    }
+
+    public void setCenterY(int y){
+        center.setY(y);
+    }
+
+    public int[] getCenterXY(){
+        int[] toReturn={center.getX(),center.getY()};
+        return toReturn;
+    }
+
+    public void setCenterXY(int x,int y){
+        center.setX(x);
+        center.setY(y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MyCircle[radius=%d ,center=%s",radius,center);
+    }
+
+    public double getArea(){
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    public double getCircumference(){
+        return 2*Math.PI*radius;
+    }
+
+    public double distance(MyCircle another){
+        return center.distance(another.center);
+    }
+}
---
+++ src/MyPointAndMyCircle/MyPoint.java
@@ -0,0 +1,57 @@
+package MyPointAndMyCircle;
+
+public class MyPoint {
+    private int x=0;
+    private int y=0;
+
+    public MyPoint() {}
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY(){
+        int[] toReturn={x,y};
+        return toReturn;
+    }
+
+    public void setXY(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d,%d)",x,y);
+    }
+
+    public double distance (int x, int y){
+        return Math.sqrt(Math.pow(this.x-x,2)+Math.pow(this.y-y,2));
+    }
+
+    public double distance (MyPoint another){
+        //pomimo ze to inny obiekt, ale jest tej samej klasy mam dostep do prywatnych pol
+        return distance(another.x,another.y);
+    }
+
+    public double distance(){
+        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    }
+}
